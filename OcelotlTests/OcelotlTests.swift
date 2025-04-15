12714b3142ee93192b8a4fdea8058e10329f75e0
Krzysztof Lam <krlam@gnhearing.com>
2025-04-15 17:48:59 +0200

    Initial Commit

---
+++ OcelotlTests/OcelotlTests.swift
@@ -0,0 +1,17 @@
+//
+//  OcelotlTests.swift
+//  OcelotlTests
+//
+//  Created by Krzysztof Lam on 15/04/2025.
+//
+
+import Testing
+@testable import Ocelotl
+
+struct OcelotlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
